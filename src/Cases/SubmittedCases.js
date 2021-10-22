95cc7117eca4312c95071169d574b2a6c18c8ce2
pranittare <tarepranit505@gmail.com>
2021-10-22 23:45:11 +0530

    html to excel

--- src/Cases/SubmittedCases.js
+++ src/Cases/SubmittedCases.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { Input } from 'reactstrap'
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 
 const SubmittedCases = (props) => {
     const [allData, setAllData] = useState([])
     const [reset, setReset] = useState(0);
+    const formatedDate = new Date().toDateString()
     const formData = (forms) => {
         let formarray = []
         const formKeys = Object.keys(forms)
@@ -65,25 +67,116 @@ const SubmittedCases = (props) => {
         setReset(Math.random())
 
     }
+    const getExcel = () => {
+        let table = document.getElementById('test-table-xls-button')
+        table.click()
+    }
+    
     useEffect(() => {
         formData(props.forms)
         // console
     }, [props.forms])
     return (
         <div>
+            <div className='d-flex justify-content-around mb-2 mt-2'>
+                
             <h4>Submitted Cases</h4>
+            <button onClick={getExcel} className='btn btn-primary'>Get Excel</button>
+            </div>
+            <ReactHTMLTableToExcel
+                id="test-table-xls-button"
+                className="d-none "
+                table="html-table"
+                filename={`Submitted-${formatedDate}`}
+                sheet="tablexls"
+                buttonText="Download as XLS" />
             <form className='d-flex justify-content-between flex-wrap'>
                 <table className="table table-striped table-bordered">
                     <thead>
                         <tr>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="appid" placeholder={'ApplicationID'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="claimedAt" placeholder={'LoginDate'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="claimedAt" placeholder={'LoginTime'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="customerName" placeholder={'CustomerName'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="tat" placeholder={'TAT'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="clientName" placeholder={'ClientName'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="TPCName1" placeholder={'Stage'} /> </th>
-                            <th scope="col"> <Input type="text" onChange={handleFilter} name="remarks" placeholder={'Remark'} /> </th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="appid" placeholder={'ApplicationID'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="claimedAt" placeholder={'LoginDate'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="claimedAt" placeholder={'LoginTime'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="customerName" placeholder={'CustomerName'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="tat" placeholder={'TAT'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="clientName" placeholder={'ClientName'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="TPCName1" placeholder={'Stage'} /></th>
+                            <th scope="col"><Input type="text" onChange={handleFilter} name="remarks" placeholder={'Remark'} /></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reset > 0 && allData && allData.length > 0 && allData.map((item, index) => {
+                            return <tr>
+                                <td>
+                                    {item.appid}
+                                </td>
+                                <td>
+                                    {
+                                        item?.office ?
+                                            item.office?.applicantDetails?.initiationDate
+                                            :
+                                            item.resident?.applicantDetails?.initiationDate
+                                    }
+
+                                </td>
+                                <td>
+                                    {
+                                        item?.office ?
+                                            item.office?.applicantDetails?.initiationDate
+                                            :
+                                            item.resident?.applicantDetails?.initiationDate
+                                    }
+
+                                </td>
+                                <td>
+                                    {
+                                        item?.office ?
+                                            item.office?.applicantDetails?.customerName
+                                            :
+                                            item.resident?.applicantDetails?.customerName
+                                    }
+                                </td>
+                                <td>
+                                    {item.tat}
+                                </td>
+                                <td>
+                                    {
+                                        item?.office ?
+                                            item.office?.applicantDetails?.bankNBFCname.clientName
+                                            :
+                                            item.resident?.applicantDetails?.bankNBFCname.clientName
+                                    }
+                                </td>
+                                <td>
+                                    Assigned {item?.assigned ? 'true' : 'false'}
+                                    Claimed {item?.claimed ? 'true' : 'false'}
+                                    Submitted {item?.submitted ? 'true' : 'false'}
+                                </td>
+                                <td>
+                                    {
+                                        item?.office ?
+                                            item.office?.applicantDetails?.remarks
+                                            :
+                                            item.resident?.applicantDetails?.remarks
+                                    }
+
+                                </td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+                {/* only for excel */}
+                <table className="table table-striped table-bordered d-none" id='html-table'>
+                    <thead>
+                        <tr>
+                            <th scope="col">ApplicationID</th>
+                            <th scope="col">LoginDate</th>
+                            <th scope="col">LoginTime</th>
+                            <th scope="col">CustomerName</th>
+                            <th scope="col">TAT</th>
+                            <th scope="col">ClientName</th>
+                            <th scope="col">Stage</th>
+                            <th scope="col">Remark</th>
                         </tr>
                     </thead>
                     <tbody>
