92cc93e2378b79deb4c0a0c7f892e893bcadb8bf
pranittare <tarepranit505@gmail.com>
2021-12-27 23:57:23 +0530

    dashboard cases today

--- src/landing/dashboard.js
+++ src/landing/dashboard.js
@@ -1,16 +1,64 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
-const Dashboard = (props) => {
+const Dashboard = ({ users, forms }) => {
 
+    const bankwise = () => {
+        let bankAndForms = { bank: [], pincode: [] }
+        for (const key in forms) {
+            if (Object.hasOwnProperty.call(forms, key)) {
+                const element = forms[key];
+                bankAndForms.pincode.push(element)
+                for (const pincode in element) {
+                    if (Object.hasOwnProperty.call(element, pincode)) {
+                        const formId = element[pincode];
+                        if (formId.office?.applicantDetails) {
+                            bankAndForms.bank.push({ name: formId?.office?.applicantDetails?.bankNBFCname.clientName, data: element })
+                        } else if (formId.resident?.applicantDetails) {
+                            bankAndForms.bank.push({ name: formId.resident?.applicantDetails?.bankNBFCname?.clientName, data: element })
+                        }
+                    }
+                }
+            }
+        }
+        return bankAndForms
+    }
+    function uniqueArray2(arr) {
+        var a = [];
+        for (var i=0, l=arr.length; i<l; i++)
+            if (a.indexOf(arr[i].name) === -1 && arr[i].name !== '')
+                a.push(arr[i].name);
+        return a;
+    }
+    const casesToday = () => {
+        let casesT = []
+        let bankname = uniqueArray2(bankwise().bank)
+        for (let index = 0; index < bankname.length; index++) {
+            const element = bankname[index];
+            casesT.push({name: element, data: []})
+        }
+        // console.log('bankname', bankname)
+        for (let i = 0; i < casesT.length; i++) {
+            const bank = casesT[i];
+            for (let index = 0; index < bankwise().bank.length; index++) {
+                const element = bankwise().bank[index];
+                if (bank.name === element.name) {
+                    bank.data.push(element)
+                }
+            }
+            
+        }
+        return casesT
+    }
     useEffect(() => {
-        console.log('dashboard redux', props)
-    }, [])
+        console.log('bankwise', bankwise())
+    }, [forms])
 
     return (
         <div className='row'>
-            <div className="col-6 bg-secondary">
+            <div className="col-6 bg-secondary p-2">
                 cases Toady
+                <button onClick={() => console.log('casesToday', casesToday())}>Test</button>
             </div>
             <div className="col-6 bg-danger">
                 Total Cases
@@ -25,7 +73,7 @@ const Dashboard = (props) => {
                 TAt
             </div>
 
-            <button value='update store' onClick={() => props.dispatch({ type: 'DATA', data: 'XYZ' })}>update redux</button>
+            {/* <button value='update store' onClick={() => props.dispatch({ type: 'DATA', data: 'XYZ' })}>update redux</button> */}
 
         </div>
     )
@@ -33,7 +81,8 @@ const Dashboard = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        userData: state.data
+        users: state.data,
+        forms: state.forms
     }
 }
 export default connect(mapStateToProps)(Dashboard);
